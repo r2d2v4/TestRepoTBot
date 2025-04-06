f17630ab53f7418ad994d2080bb2304bfe028a6e
R2D2V4 <r2d2v4@gmail.com>
2025-04-06 19:48:37 +0300

    Доработки, публикация на vds

--- tbot_2.py
+++ tbot_2.py
@@ -3,7 +3,6 @@ import tbot_2_lib
 import os
 from dotenv import load_dotenv
 from datetime import datetime as dt
-import psycopg
 
 
 load_dotenv()
@@ -13,30 +12,24 @@ bot = telebot.TeleBot(token)
 
 
 def monitoring_standard(message):
-    #print(f"Пользователь: {message.from_user.username}")  # это интересно :)
-    #print(f"Ввел текст: {message.text}")
-    #print(f"Дата: {dt.fromtimestamp(message.date).strftime('%Y-%m-%d %H:%M:%S')}")
     return [dt.fromtimestamp(message.date).strftime('%Y-%m-%d %H:%M:%S'), message.from_user.username[:100], message.text[:255]]
 
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
-    #monitoring_standard(message)
     start_message = "Напиши два числа церез пробел, а я посчитаю их сумму."
     bot.send_message(message.chat.id, start_message)
     hello = monitoring_standard(message)
     hello.append(start_message)
     tbot_2_lib.postgres_add_line(hello)
-    #print(f"Бот ответил: {start_message}\n")
 
 @bot.message_handler(commands=['help'])
 def help_command(message):
     hello = monitoring_standard(message)
     help_message = "Вот список команд, которые мне доступны:\n/start\n/help\n/about"
     bot.send_message(message.chat.id, help_message)
-    hello.append(help_message)
+    hello.append(help_message.replace("\n", " "))
     tbot_2_lib.postgres_add_line(hello)
-    #print(f"Бот ответил: {help_message}\n")
 
 @bot.message_handler(commands=['about'])
 def samsa_command(message):
@@ -47,9 +40,8 @@ def samsa_command(message):
                     "И тогда я смогу приносить больше пользы! "\
                     "Давай попробуем! /start"
     bot.send_message(message.chat.id, about_message)
-    hello.append(about_message)
+    hello.append(about_message.replace("\n", " "))
     tbot_2_lib.postgres_add_line(hello)
-    #print(f"Бот ответил: {about_message}\n")
 
 @bot.message_handler(content_types = ['text'])
 def text_message(message):
@@ -57,22 +49,19 @@ def text_message(message):
     if message.text.lower() == 'привет':
         hello_message = "Приветствую тебя! Давай начнём! /start"
         bot.send_message(message.chat.id, hello_message)
-        hello.append(hello_message)
+        hello.append(hello_message.replace("\n", " "))
         tbot_2_lib.postgres_add_line(hello)
-        #print(f"Бот ответил: {hello_message}\n")
     elif len(message.text.split()) == 2:
         number_a, number_b = message.text.split()
         answer = tbot_2_lib.add_number(number_a, number_b)
         bot.send_message(message.chat.id, answer)
-        hello.append(str(answer)[:255])
+        hello.append(str(answer)[:255].replace("\n", " "))
         tbot_2_lib.postgres_add_line(hello)
-        #print(f"Бот ответил: {answer}\n")
-
     else:
         other_message = tbot_2_lib.rnd_text()
         bot.send_message(message.chat.id, other_message)
-        hello.append(other_message)
+        hello.append(other_message.replace("\n", " "))
         tbot_2_lib.postgres_add_line(hello)
-        #print(f"Бот ответил: {other_message}\n")
 
-bot.polling(none_stop=True, interval=0)
+#bot.polling(none_stop=True, interval=0)
+bot.polling()
--- tbot_2_lib.py
+++ tbot_2_lib.py
@@ -52,72 +52,16 @@ db_host = os.getenv("DB_HOST")
 db_user = os.getenv("DB_USER")
 db_password = os.getenv("DB_PASSWORD")
 db_name = os.getenv("DB_NAME")
+
+
 def postgres_add_line(text):
-    connect = psycopg.connect(host=db_host, user=db_user, password=db_password, dbname=db_name)
+    connect = psycopg.connect(host=db_host, port=5432, user=db_user, password=db_password, dbname=db_name)
     cursor = connect.cursor()
     cursor.execute("INSERT INTO tbot2log (datetime, username, question, answer) VALUES (%s, %s, %s, %s);", text)
     connect.commit()
     cursor.close()
     connect.close()
 
-
-'''
-def bar(number):
-    result = 0
-    try:
-        result = int(number)
-    except ValueError:
-        pass
-
-    if not result:
-        try:
-            result = float(number)
-        except ValueError:
-            result = 'LoLo'
-
-    return result
-'''
-
-
-
-
-
-
-
-
-'''
-def bar(number):
-    if number[0] == '+' and positive_number(number[1:]):
-        #print('Первое слагаемое - положительное число типа int, начинается с символа +')
-        a = int(number)
-    elif number[0].isdigit() and positive_number(number):
-        #print('Первое слагаемое - положительное число типа int.')
-        a = int(number)
-    elif number[0] == '-' and positive_number(number[1:]):
-        #print('Первое слагаемое - отрицательное число типа int, начинается с символа -')
-        a = int(number)
-    elif number[0] == '+' and (delimeter_point1(number[1:]) or delimeter_point2(number[1:])):
-        if delimeter_point2(number[1:]):
-            number = number.replace(',', '.')
-        #print("Первое слагаемое - положительное число типа float, начинается с символа +")
-        a = float(number)
-    elif number[0] == '-' and (delimeter_point1(number[1:]) or delimeter_point2(number[1:])):
-        if delimeter_point2(number[1:]):
-            number = number.replace(',', '.')
-        #print('Первое слагаемое - отрицательное число типа float начинается с символа -')
-        a = float(number)
-    elif positive_number(number):
-        #print('Первое слагаемое - положительное число типа int')
-        a = int(number)
-    elif delimeter_point1(number) or delimeter_point2(number):
-        if delimeter_point2(number):
-            number = number.replace(',', '.')
-        #print('Первое слагаемое - положительное число типа float')
-        a = float(number)
-    else:
-        a = number
-    return a
-'''
 
 def rnd_text():
     words = ["Что-то тут не так. Попробуй снова.\nИли напиши /help для справки.",
